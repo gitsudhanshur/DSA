ac9fc7631940ce8c49ab0f49a1a8cd79718aeb57
gitsudhanshur <sudhanshuranjan715@gmail.com>
2025-03-26 21:00:13 +0530

    new code

---
+++ src/Recursion/Print_1_to_n.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+public class Print_1_to_n {
+    public static void main(String[] args) {
+        funBoth(5);
+    }
+
+    static void funRev(int n) {
+        if (n == 0) {
+            return;
+        }
+        funRev(n-1);
+        System.out.println(n);
+    }
+
+    static void funBoth(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        funBoth(n-1);
+        System.out.println(n);
+    }
+}
---
+++ src/Recursion/Print_n_to_1.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class Print_n_to_1 {
+    public static void main(String[] args) {
+        fun(5);
+    }
+
+    static void fun(int n) {
+        if (n == 0) {
+            return;
+        }
+
+        System.out.println(n);
+        fun(n-1);
+    }
+}
